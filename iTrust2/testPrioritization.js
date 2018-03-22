a15f60b71b581c30feb5238f1425221f11167b5d
krkulkar <krkulkar@ncsu.edu>
2018-03-22 16:55:09 -0400

    Correct test prioritization

--- iTrust2/testPrioritization.js
+++ iTrust2/testPrioritization.js
@@ -3,40 +3,54 @@ var fs = require('fs'),
     child  = require('child_process'); 
 var parser = new xml2js.Parser();
 var Bluebird = require('bluebird')
+var HashMap = require('hashmap')
+var allTestsMap = new HashMap();
 
 calculatePriority();
 
 function readResults(result)
 {
-    var duration = result.build.duration;
-    var res = result.build.result;
-    var commitID = result.build.actions[0]["hudson.plugins.git.RevisionParameterAction"][0]["commit"];
-    
-    var tests = {
-        commitID: commitID,
-        duration: duration, 
-        res: res
-    };
-        
-    return tests;
+    var tests = [];
+    for (var i = 0; i < result.testsuite.testcase.length; i++) {
+        var name = result.testsuite.testcase[i].$.classname + ":" + result.testsuite.testcase[i].$.name;
+        var totalTime = parseFloat(result.testsuite.testcase[i].$.time);
+        var failureCount = ((result.testsuite.testcase[i].failure == null) ? 0 : 1);
+        if (allTestsMap.has(name)) {
+            var test = allTestsMap.get(name);
+            var newTotalTime = test["totalTime"] + totalTime;
+            var newFailureCount = test["failureCount"] + failureCount;
+            allTestsMap.set(name, {"totalTime": newTotalTime, "failureCount": newFailureCount, "name": name});
+        }
+        else {
+            allTestsMap.set(name, {"totalTime": totalTime, "failureCount": failureCount, "name": name});
+        }
+    }    
+    return null;
 }
 
 async function calculatePriority()
 {
-    var tests = [];
-    for(i = 100; i <= 150; i++){
-        var testReport =  '/var/lib/jenkins/jobs/iTrust2/builds/' + i + '/build.xml';
-        var contents = fs.readFileSync(testReport)
-        let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
-        tests[i-1] = readResults(xml2json);
+    for(i = 1; i <= 100; i++){
+       var files = fs.readdirSync('/var/lib/jenkins/jobs/iTrust2/builds/' + i + '/surefire-reports/');
+       for (var j in files) {
+         var testReport = '/var/lib/jenkins/jobs/iTrust2/builds/' + i + '/surefire-reports/' + files[j];
+         var contents = fs.readFileSync(testReport)
+         let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
+         readResults(xml2json);
+       }        
      }
-    
-    tests.sort(function(a,b){
-    	if(a.res == "FAILURE" && b.res == "SUCCESS") return -1;
-    	if(b.res == "FAILURE" && a.res == "SUCCESS") return 1;
-    	if(a.res[0] === b.res[0]) return a.duration - b.duration;
+
+     var array_values = new Array();
+     allTestsMap.forEach(function(value,key){
+       array_values.push(allTestsMap.get(key));
+     })
+        
+     array_values.sort(function(a,b){
+        if(a.failureCount > b.failureCount) return -1;
+        if(a.failureCount < b.failureCount) return 1;
+        if(a.failureCount === b.failureCount) return a.totalTime - b.totalTime;
      })
 
-    tests.forEach( e => console.log(e));
+    array_values.forEach( e => console.log(e));
     return tests;
 }
